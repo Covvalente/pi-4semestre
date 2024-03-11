c4134da8f356cf36eee5050cabdb1859d47262b0
Mateus Rodrigues De Oliveira <97472046+Covvalente@users.noreply.github.com>
2024-03-11 10:30:31 -0300

    Add files via upload

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,70 +1,19 @@
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        window.location.href = "../transaction/transaction.html";
-    }
-})
-
-
-// Esta função recebe um email como entrada e retorna verdadeiro se estiver em um formato válido e falso caso contrário
-function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
-
-// Esta função é chamada quando o campo de entrada de email muda. Ela verifica se o email é válido e exibe mensagens de erro de acordo
-function onChangeEmail() {
-    const email = form.email().value;
-    form.emailRequiredError().style.display = email ? "none" : "block";
-
-    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
-
-    toggleRegisterButtonDisable();
-}
-
-// Esta função é chamada quando o campo de entrada de senha muda. Ela verifica se a senha atende aos requisitos e exibe mensagens de erro de acordo
-function onChangePassword() {
-    const password = form.password().value;
-    form.passwordRequiredError().style.display = password ? "none" : "block";
-
-    form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
-
-    validatePasswordsMatch();
-    toggleRegisterButtonDisable();
-}
-
-// Esta função é chamada quando o campo de entrada de confirmação de senha muda. Ela verifica se as senhas coincidem e exibe uma mensagem de erro se não coincidirem
-function onChangeConfirmPassword() {
-    validatePasswordsMatch();
-    toggleRegisterButtonDisable();
-}
-
 // Função para registrar um usuário
 function register() {
-
     // Obtém o valor do email e senha do formulário 
     const email = form.email().value;
     const password = form.password().value;
+
     // Cria um usuário com email e senha usando o Firebase
-    firebase.auth().createUserWithEmailAndPassword(
-        email, password
-    ).then(() => {
-
-        // Redireciona para a página inicial após o registro bem-sucedido
-        window.location.href = "../../pages/transaction/transaction.html";
-    }).catch(error => {
-
-        // Exibe uma mensagem de erro se houver algum problema
-        alert(getErrorMessage(error));
-    })
-}
-
-// Função para obter a mensagem de erro apropriada
-function getErrorMessage(error) {
-    // Verifica se o erro é devido ao email já estar em uso
-    if (error.code == "auth/email-already-in-use") {
-        return "Email já está em uso";
-    }
-    // Retorna a mensagem de erro padrão
-    return error.message;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            // Redireciona para a página inicial após o registro bem-sucedido
+            window.location.href = "../../pages/transaction/transaction.html";
+        })
+        .catch(error => {
+            // Exibe uma mensagem de erro se houver algum problema
+            alert(getErrorMessage(error));
+        });
 }
 
 // Esta função verifica se os campos de senha e confirmação de senha coincidem e exibe uma mensagem de erro se não coincidirem
@@ -101,6 +50,34 @@ function isFormValid() {
     return true;
 }
 
+// Esta função recebe um email como entrada e retorna verdadeiro se estiver em um formato válido e falso caso contrário
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+// Esta função é chamada quando o campo de entrada de email muda. Ela verifica se o email é válido e exibe mensagens de erro de acordo
+function onChangeEmail() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+    toggleRegisterButtonDisable();
+}
+
+// Esta função é chamada quando o campo de entrada de senha muda. Ela verifica se a senha atende aos requisitos e exibe mensagens de erro de acordo
+function onChangePassword() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+    form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+}
+
+// Esta função é chamada quando o campo de entrada de confirmação de senha muda. Ela verifica se as senhas coincidem e exibe uma mensagem de erro se não coincidirem
+function onChangeConfirmPassword() {
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+}
+
 const form = {
     // Obter o elemento de confirmação de senha
     confirmPassword: () => document.getElementById('confirmPassword'),
@@ -121,6 +98,7 @@ const form = {
     // Obter o elemento do botão registrar
     registerButton: () => document.getElementById('register-button')
 }
+
 function VoltarRegistro() {
-    window.history.back()
+    window.history.back();
 }
